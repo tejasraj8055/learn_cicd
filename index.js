a02be5c8d4718b2efec5bff956e6490398c8dc77
Tejas <vtejasraj@gmail.com>
2024-05-25 16:32:56 +0530

    added error handler

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     }
 )
 
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+    }
+)
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
     }
