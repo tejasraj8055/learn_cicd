1ea00dca2a5a95e05d5b34a34c214b71579a1432
Tejas <vtejasraj@gmail.com>
2024-05-25 16:30:19 +0530

    added build.yaml

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+
+const PORT = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    }
+)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+    }
+)
